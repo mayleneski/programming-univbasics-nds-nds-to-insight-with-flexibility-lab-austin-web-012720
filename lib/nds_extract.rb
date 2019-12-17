537668ae2395f1bdc16f5234bcbaa70064686101
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-17 03:06:47 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,8 @@
 # Provided, don't edit
 require 'directors_database'
+require 'pp'
+require 'pry'
+
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
@@ -34,6 +37,17 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
+  
+  movie_array_with_director_key = []
+  i = 0
+  
+  while i < movies_collection.length do
+    movie_array_with_director_key << movie_with_director_name(name, movies_collection[i])
+    i += 1
+  end
+  
+  movie_array_with_director_key
+  
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
   # and accumulate the returned Array of movies into a new Array that's
@@ -52,6 +66,23 @@ end
 
 
 def gross_per_studio(collection)
+  
+  gross_per_studio_hash = {}
+  
+  i = 0
+  while i < collection.length do
+    if !gross_per_studio_hash[collection[i][:studio]]
+      
+      gross_per_studio_hash[collection[i][:studio]] = collection[i][:worldwide_gross]
+    else
+      gross_per_studio_hash[collection[i][:studio]] += collection[i][:worldwide_gross]
+      
+    end
+   i += 1
+  end
+  
+  gross_per_studio_hash
+  
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
@@ -66,6 +97,17 @@ def gross_per_studio(collection)
 end
 
 def movies_with_directors_set(source)
+  
+  directors_set_array = []
+  i = 0
+  
+  while i < source.length do
+    directors_set_array << movies_with_director_key(source[i][:name], source[i][:movies])
+    i += 1
+  end  
+  
+  directors_set_array
+  
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
   # INPUT:
